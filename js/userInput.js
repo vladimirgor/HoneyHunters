d901f35f915115fb99e675ad25112ff161501956
vladimirgor <vladimirgor2011@yandex.ru>
2017-04-13 20:09:37 +0500

    init git

---
+++ js/userInput.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+$("form").submit(function(event){
+    var name = $("#name").val();
+    var comment = $("#comment").val();
+    var email = $("#email").val();
+    event.preventDefault();
+    $.getJSON('php/InsertSelect.php',
+        { name : name, comment : comment, email : email },
+        processResult
+    );
+});
+
+});
+function processResult(json)	{
+    var str = '';
+    var j = 0;
+    $("#name").val("");
+    $("#comment").val("");
+    $("#email").val("");
+    $("#list").html("");
+    $.each(json, function (i,e) {
+        if ( j % 3 == 0  ) $("#list").append('<div class = "col-sm-1"></div>');
+        j++;
+        if ( i % 2 == 0 ) str = 'c'; else str = 'nc';
+        $("#list").append(
+        '<div class="col-sm-3 ticket' +str+ '">' +
+        '<p class="'+str+'1'+'">' + e.name + '</p>'+
+        '<p class="'+str+'2'+ '">' + e.email + '</p>'+
+        '<p class="'+str+'3'+'">'+ e.comment + '</p>'+
+        '</div>');
+    });
+}
+
+
---
+++ php/InsertSelect.php
@@ -0,0 +1,14 @@
+<?php
+include_once('config.php');
+require_once('functions.php');
+$name = $_GET['name'];
+$comment = $_GET['comment'];
+$email = $_GET['email'];
+
+$link=dbLink();
+if (!$name == "")
+Insert(TABLE,['name' => $name,'comment' => $comment,'email' => $email],$link);
+$sql="SELECT * FROM ". TABLE ." ORDER BY id DESC";
+$comment_list=Select($sql,$link);
+echo json_encode($comment_list);
+
