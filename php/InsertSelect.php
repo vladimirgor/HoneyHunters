94b81b71a05b8c05972998936755fa7641b3605b
vladimirgor <vladimirgor2011@yandex.ru>
2017-04-14 09:57:34 +0500

    check session from ajax and session_id and empty name comment email

--- php/InsertSelect.php
+++ php/InsertSelect.php
@@ -1,14 +1,24 @@
 <?php
+session_start();
 include_once('config.php');
 require_once('functions.php');
 $name = $_GET['name'];
 $comment = $_GET['comment'];
 $email = $_GET['email'];
-
+$session = $_GET['session'];
+if ( $session != session_id() ) {
+    $message[] = ['message' => "Возможно предпринята XSS атака." ];
+    echo json_encode($message);
+    die;
+}
 $link=dbLink();
-if ( $name != "" )
-Insert(TABLE,['name' => $name,'comment' => $comment,'email' => $email],$link);
-$sql="SELECT * FROM ". TABLE ." ORDER BY id DESC";
-$comment_list=Select($sql,$link);
-echo json_encode($comment_list);
-
+if ( $name == "" || $comment == "" || $email == "" ) {
+    $message[] = ['message' => "Все данные должны быть непустыми." ];
+    echo json_encode($message);
+    die;
+} else {
+    Insert(TABLE, ['name' => $name, 'comment' => $comment, 'email' => $email], $link);
+    $sql = "SELECT * FROM " . TABLE . " ORDER BY id DESC";
+    $comment_list = Select($sql, $link);
+    echo json_encode($comment_list);
+}
