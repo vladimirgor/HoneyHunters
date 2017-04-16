53d8d52d05871563830c7940d350283895b6ff63
vladimirgor <vladimirgor2011@yandex.ru>
2017-04-16 23:19:43 +0500

    rechange

--- index.php
+++ index.php
@@ -39,17 +39,17 @@ session_start();
         <div class="row form" id="write">
             <div class="col-sm-3 col-sm-offset-1">
                 Имя <span class="star">&#9733;</span><br>
-                <input type="text"  required autofocus pattern="^[A-ZА-ЯЁ][А-Яа-яЁёA-Za-z\s]+$"  id="name">
+                <input type="text"  required  autofocus pattern="^[A-ZА-ЯЁ][А-Яа-яЁёA-Za-z\s]+$"  id="name">
             </div>
             <div class="col-sm-3 col-sm-offset-2">
                 Комментарий <span class="star">&#9733;</span><br>
-                <input type="text"   required pattern="^[A-ZА-ЯЁ][А-Яа-яЁёA-Za-z!\-,.\s&#34;]+$" id="comment">
+                <input type="text"   required pattern="^[A-ZА-ЯЁ][А-Яа-яЁёA-Za-z!\-,.\s&#34;]+$"  id="comment">
             </div>
         </div>
         <div class="row form">
             <div class="col-sm-3 col-sm-offset-1">
                 E-Mail <span class="star">&#9733;</span><br>
-                <input type="email" required id="email">
+                <input type="email"  required id="email">
             </div>
         </div>
         <input hidden type="text"  id="session" value ="<?=session_id()?>">
