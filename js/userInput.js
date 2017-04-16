7293d3302f5ceec19ab9d5e0ad8ef444b0a9b9b2
vladimirgor <vladimirgor2011@yandex.ru>
2017-04-16 20:00:44 +0500

    comments added

--- js/userInput.js
+++ js/userInput.js
@@ -5,31 +5,36 @@ $(document).ready(function(){
         var comment = $("#comment").val();
         var email = $("#email").val();
         var session = $("#session").val();
-        event.preventDefault();
-        $.getJSON('../php/InsertSelect.php',
+        event.preventDefault();                   /*  page reload avoiding */
+        $.getJSON('../php/InsertSelect.php',      /*  ajax request sending */
             { name : name, comment : comment, email : email, session : session },
             processResult
         );
     });
 
 });
-
+/*  ajax response processing  */
 function processResult(json)	{
     var str = '';
     var j = 0;
+/* input fields value cleaning */
     $("#name").val("");
     $("#comment").val("");
     $("#email").val("");
+/* cards html cleaning */
     $("#list").html("");
 
     $.each(json, function (i,e) {
 
         if ( typeof(e.message) == 'string' ) {
+/* message rendering */
             $('#message').html(e.message);
         } else {
+/* cards  html forming */
             if (j % 3 == 0) $("#list").append('<div class = "col-sm-1"></div>');
             j++;
-            if (i % 2 == 0) str = 'c'; else str = 'nc';
+            if (i % 2 == 0) str = 'c'; /* even card class */
+            else str = 'nc';           /* odd  card class */
             $("#list").append(
                 '<div class="col-sm-3 ticket' + str + '">' +
                 '<p class="' + str + '1' + '">' + e.name + '</p>' +
